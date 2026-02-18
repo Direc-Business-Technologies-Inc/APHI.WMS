60048691b2eaa4c7d72d0f5bf1d61998dbb94125
i-nicolas <tbsi.nicolas@gmail.com>
2026-02-19 01:35:10 +0800

    feat: base refs on sap ref DTO

--- Application.DataTransferObjects/Transactions/Commons/SapDocumentReferenceDTO.cs
+++ Application.DataTransferObjects/Transactions/Commons/SapDocumentReferenceDTO.cs
@@ -2,6 +2,8 @@
 
 public class SapDocumentReferenceDTO
 {
-    public string DocNum { get; set; } = string.Empty;
-    public string DocEntry { get; set; } = string.Empty;
+    public int DocNum { get; set; }
+    public int DocEntry { get; set; }
+    public int BaseEntry { get; set; }
+    public int BaseDocNum { get; set; }
 }
