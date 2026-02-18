7c19ed6efaa70df002a0c806da3f88203ef513c9
i-nicolas <tbsi.nicolas@gmail.com>
2026-02-19 01:35:30 +0800

    feat: receiving SAP DTO

---
+++ Application.DataTransferObjects/Transactions/Receiving/SAP/PurchaseDeliveryNoteLineSAPDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Integration.SAP.Entities.Transactional.Receiving;
+
+public class PurchaseDeliveryNoteLineSAPDTO
+{
+    public int DocEntry { get; set; }
+    public int DocNum { get; set; }
+    public string LineNum { get; set; }
+    public string ItemCode { get; set; }
+    public string ItemName { get; set; }
+    public decimal Quantity { get; set; }
+    public string UoMCode { get; set; }
+    public decimal UoMValue { get; set; }
+    public string UoMName { get; set; }
+    public string InputType { get; set; }
+}
