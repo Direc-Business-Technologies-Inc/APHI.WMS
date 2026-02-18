c7089c25e7f5283d74fa46a496077e5ddd27af7e
i-nicolas <tbsi.nicolas@gmail.com>
2026-02-19 01:39:04 +0800

    fix: various UI adjustments

--- Web.BlazorServer/Components/Security/AppAuthenticationService.cs
+++ Web.BlazorServer/Components/Security/AppAuthenticationService.cs
@@ -1,5 +1,7 @@
-﻿using System.Security.Claims;
+﻿using Shared.Services.Repository;
+using System.Security.Claims;
 using System.Text.Json;
+using Web.BlazorServer.Services.Implementation;
 
 namespace Web.BlazorServer.Components.Security;
 
