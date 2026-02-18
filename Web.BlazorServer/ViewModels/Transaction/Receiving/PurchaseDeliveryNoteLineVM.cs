1a9f85d3e26b964215c8db8fb473eeaaf0537b33
i-nicolas <tbsi.nicolas@gmail.com>
2026-02-19 01:37:29 +0800

    feat: receiving VM

---
+++ Web.BlazorServer/ViewModels/Transaction/Receiving/PurchaseDeliveryNoteLineVM.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities.Enums.Transaction.Receiving;
+using Web.BlazorServer.ViewModels.Others;
+using Web.BlazorServer.ViewModels.Transaction.Commons;
+
+namespace Web.BlazorServer.ViewModels.Transaction.Receiving;
+
+public class PurchaseDeliveryNoteLineVM : ItemVM
+{
+    public int DocEntry { get; set; }
+    public int DocNum { get; set; }
+    public int BaseEntry { get; set; }
+    public int BaseDocNum { get; set; }
+    public int BaseLine { get; set; }
+    public string TaxCode { get; set; }
+    public WarehouseVM Warehouse { get; set; }
+    public InputType InputType { get; set; }
+}
