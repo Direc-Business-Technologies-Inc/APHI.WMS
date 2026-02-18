3b0d4fe84d6fd5c1610987a67f88a3b931eb8f2b
i-nicolas <tbsi.nicolas@gmail.com>
2026-02-19 01:34:14 +0800

    feat: GRPO Posting JSON

---
+++ Integration.SAP/Entities/Transactional/Receiving/GoodsReceiptPO.cs
@@ -0,0 +1,30 @@
+﻿using Ardalis.GuardClauses;
+
+namespace Integration.SAP.Entities.Transactional.Receiving;
+
+public class GoodsReceiptPO
+{
+    public string CardCode { get; private set; }
+    public DateTime DocDate { get; private set; }
+    public DateTime DocDueDate { get; private set; }
+    public DateTime TaxDate { get; private set; }
+    public string U_RecBy { get; private set; }
+    public string Comments { get; private set; }
+    public List<GoodsReceiptPOLines> DocumentLines { get; private set; } = [];
+
+    public GoodsReceiptPO(string cardCode,
+                          DateTime docDate,
+                          DateTime docDueDate,
+                          DateTime taxDate,
+                          string recBy,
+                          List<GoodsReceiptPOLines> documentLines)
+    {
+        CardCode = Guard.Against.NullOrEmpty(cardCode, nameof(CardCode), "Card Code cannot be null or empty");
+        DocDate = Guard.Against.NullOrOutOfSQLDateRange(docDate, nameof(DocDate), "Doc Date cannot be null or out of range");
+        DocDueDate = Guard.Against.NullOrOutOfSQLDateRange(docDueDate, nameof(DocDueDate), "Doc Due Date cannot be null or out of range");
+        TaxDate = Guard.Against.NullOrOutOfSQLDateRange(taxDate, nameof(TaxDate), "Tax Date cannot be null or out of range");
+        U_RecBy = Guard.Against.NullOrEmpty(recBy, nameof(U_RecBy), "Received By cannot be null or empty");
+        DocumentLines = [.. Guard.Against.NullOrEmpty(documentLines, nameof(DocumentLines), "Document Lines cannot be null or empty")];
+        Comments = "Posted from WMS";
+    }
+}
---
+++ Integration.SAP/Entities/Transactional/Receiving/GoodsReceiptPOLines.cs
@@ -0,0 +1,37 @@
+﻿using Ardalis.GuardClauses;
+
+namespace Integration.SAP.Entities.Transactional.Receiving;
+
+public class GoodsReceiptPOLines
+{
+    public int BaseEntry { get; private set; }
+    public int BaseType { get; private set; }
+    public int BaseLine { get; private set; }
+    public int LineNum { get; private set; }
+    public string ItemCode { get; private set; }
+    public decimal Quantity { get; private set; }
+    public string TaxCode { get; private set; }
+    public string WarehouseCode { get; private set; }
+    public string U_InputType { get; private set; }
+
+    public GoodsReceiptPOLines(int baseEntry,
+                               int baseType,
+                               int baseLine,
+                               int lineNum,
+                               string itemCode,
+                               decimal qty,
+                               string taxCode,
+                               string whsCode,
+                               string inputType)
+    {
+        BaseEntry = Guard.Against.Negative(baseEntry, nameof(BaseEntry), "Base Entry cannot be negative or null");
+        BaseType = Guard.Against.Negative(baseType, nameof(BaseType), "Base Type cannot be negative or null");
+        BaseLine = Guard.Against.Negative(baseLine - 1, nameof(BaseLine), "Base Line cannot be negative or null");
+        LineNum = Guard.Against.Negative(lineNum, nameof(LineNum), "Base Line cannot be negative or null");
+        ItemCode = Guard.Against.NullOrEmpty(itemCode, nameof(ItemCode), "Item Code cannot be null or empty");
+        Quantity = Guard.Against.NegativeOrZero(qty, nameof(Quantity), "Quantity cannot be negative or zero");
+        TaxCode = Guard.Against.NullOrEmpty(taxCode, nameof(TaxCode), "Tax Code cannot be null or empty");
+        WarehouseCode = Guard.Against.NullOrEmpty(whsCode, nameof(WarehouseCode), "Warehouse Code cannot be null or empty");
+        U_InputType = Guard.Against.NullOrEmpty(inputType, nameof(U_InputType), "Input Type cannot be null or empty");
+    }
+}
