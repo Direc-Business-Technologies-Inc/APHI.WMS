771eb488fafa69a43f96bb4c3dc5b89a411fb14a
i-nicolas <tbsi.nicolas@gmail.com>
2026-02-19 01:37:24 +0800

    feat: various VM adjustments from DTO and DEM changes

---
+++ Web.BlazorServer/ViewModels/Transaction/Commons/TransactionalDocumentVM.cs
@@ -0,0 +1,13 @@
+﻿using Application.DataTransferObjects.System.Commons;
+using Application.DataTransferObjects.Transactions.Commons;
+using Domain.Enums.Transaction.Commons;
+
+namespace Web.BlazorServer.ViewModels.Transaction.Commons;
+
+public class TransactionalDocumentVM
+{
+    public ApprovalStatus ApprovalStatus { get; set; } = ApprovalStatus.None;
+    public AppDocNumVM AppDocNum { get; set; } = new();
+    public SapDocumentReferenceVM SapReference { get; set; } = new();
+    public DocumentTypeVM DocumentType { get; set; } = new();
+}
