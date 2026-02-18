bd60de3571c2ebe82b885cdc1f23a47ee5489510
i-nicolas <tbsi.nicolas@gmail.com>
2026-02-19 01:39:28 +0800

    feat: receiving page index

---
+++ Web.BlazorServer/Components/Pages/Transaction/Receiving/ReceivingPage.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Web.BlazorServer.Components.Pages.Transaction.Receiving;
+
+public partial class ReceivingPage
+{
+    #region Parameters
+    [SupplyParameterFromQuery]
+    [Parameter] public string T { get; set; } = "po";
+    #endregion Parameters
+
+    #region Primitives
+    int SelectedTab { get; set; } = 0;
+    #endregion Primitives
+
+    #region Custom Functions
+    void TabChanged() => T = SelectedTab == 0 ? "po" : "grpo";
+    #endregion Custom Functions
+}
