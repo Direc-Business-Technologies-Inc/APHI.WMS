8e5f9c3a6ca1fc78a5a1d45af6fe7c63578192ea
i-nicolas <tbsi.nicolas@gmail.com>
2026-02-24 10:19:51 +0800

    feat: goods issue VM

---
+++ Web.BlazorServer/ViewModels/Transaction/GoodsIssue/GoodsIssueLineVM.cs
@@ -0,0 +1,10 @@
+﻿using Application.DataTransferObjects.Others;
+using Web.BlazorServer.ViewModels.Others;
+using Web.BlazorServer.ViewModels.Transaction.Commons;
+
+namespace Web.BlazorServer.ViewModels.Transaction.GoodsIssue;
+
+public class GoodsIssueLineVM : ItemVM
+{
+    public WarehouseVM Warehouse { get; set; } = new();
+}
---
+++ Web.BlazorServer/ViewModels/Transaction/GoodsIssue/GoodsIssueVM.cs
@@ -0,0 +1,12 @@
+﻿using Web.BlazorServer.ViewModels.Others;
+using Web.BlazorServer.ViewModels.Transaction.Commons;
+
+namespace Web.BlazorServer.ViewModels.Transaction.GoodsIssue;
+
+public class GoodsIssueVM : MarketingDocumentVM
+{
+    public string PreparedBy { get; set; } = string.Empty;
+    public TransactionTypeVM TransactionType { get; set; } = new();
+    public WarehouseVM Warehouse { get; set; } = new();
+    public IEnumerable<GoodsIssueLineVM> DocumentLines { get; set; } = [];
+}
