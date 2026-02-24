fce0e723ae001c263086e892f6fd13cc629c0ec6
i-nicolas <tbsi.nicolas@gmail.com>
2026-02-24 10:21:56 +0800

    feat: goods receipt UI components

---
+++ Web.BlazorServer/Components/Pages/Transaction/GoodsReceipt/GoodsReceiptPage.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Web.BlazorServer.Components.Pages.Transaction.GoodsReceipt;
+
+public partial class GoodsReceiptPage
+{
+    #region Parameters
+    [SupplyParameterFromQuery]
+    [Parameter] public string T { get; set; } = "aprvd";
+    #endregion Parameters
+
+    #region Primitives
+    int SelectedTab { get; set; } = 0;
+    #endregion Primitives
+
+    #region Overrides
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+        if (T is not null)
+            SelectedTab = T.ToLower() == "aprvd" ? 0 : T.ToLower() == "pndng" ? 1 : 2;
+    }
+
+    #endregion Overrides
+
+    #region Custom Functions
+    void TabChanged()
+    {
+        T = SelectedTab == 0 ? "aprvd" : SelectedTab == 1 ? "pndng" : "rjct";
+        NavManager.NavigateTo($"/transactions/inventory/goods-receipt?T={T}");
+    }
+    #endregion Custom Functions
+}
