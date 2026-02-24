4a59f2ee86810ae130300bb35657bbf290384415
i-nicolas <tbsi.nicolas@gmail.com>
2026-02-24 10:21:14 +0800

    feat: goods issue UI handler

--- Web.BlazorServer/Handlers/BlazorServerHandlersDI.cs
+++ Web.BlazorServer/Handlers/BlazorServerHandlersDI.cs
@@ -4,6 +4,7 @@ using Web.BlazorServer.Handlers.Implementations.Administration.Role;
 using Web.BlazorServer.Handlers.Implementations.Administration.User;
 using Web.BlazorServer.Handlers.Implementations.Others;
 using Web.BlazorServer.Handlers.Implementations.System;
+using Web.BlazorServer.Handlers.Implementations.Transaction.GoodsIssue;
 using Web.BlazorServer.Handlers.Implementations.Transaction.GoodsReturn;
 using Web.BlazorServer.Handlers.Implementations.Transaction.Receiving;
 using Web.BlazorServer.Handlers.Repositories.Administration.Authorization;
@@ -11,6 +12,7 @@ using Web.BlazorServer.Handlers.Repositories.Administration.Role;
 using Web.BlazorServer.Handlers.Repositories.Administration.User;
 using Web.BlazorServer.Handlers.Repositories.Others;
 using Web.BlazorServer.Handlers.Repositories.System;
+using Web.BlazorServer.Handlers.Repositories.Transaction.GoodsIssue;
 using Web.BlazorServer.Handlers.Repositories.Transaction.GoodsReturn;
 using Web.BlazorServer.Handlers.Repositories.Transaction.Receiving;
 
@@ -32,6 +34,7 @@ public static class BlazorServerHandlersDI
         services.TryAddTransient<IBusinessPartnerHandler, BusinessPartnerHandler>();
         services.TryAddTransient<IItemMasterDataHandler, ItemMasterDataHandler>();
         services.TryAddTransient<IWarehouseMasterDataHandler, WarehouseMasterDataHandler>();
+        services.TryAddTransient<IGoodsIssueHandler, GoodsIssueHandler>();
         services.TryAddTransient<ITransactionTypeHandler, TransactionTypeHandler>();
 
         return services;
