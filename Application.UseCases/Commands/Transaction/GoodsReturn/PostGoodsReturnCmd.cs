1d1231e17cd0fab38161bc2e17404cd29a221c07
i-nicolas <tbsi.nicolas@gmail.com>
2026-02-24 09:38:59 +0800

    feat: post goods return cmd

---
+++ Application.UseCases/Commands/Transaction/GoodsReturn/PostGoodsReturnCmd.cs
@@ -0,0 +1,22 @@
+﻿using Application.DataTransferObjects.Transactions.GoodsReturn;
+using Application.UseCases.Repositories.Integration.Transaction.GoodsReturn;
+using Mapster;
+using MediatR;
+
+namespace Application.UseCases.Commands.Transaction.GoodsReturn;
+
+public record PostGoodsReturnCmd(GoodsReturnRequestDTO Data) : IRequest<bool>;
+
+public class PostGoodsReturnCmdHandler(
+    IGoodsReturnIntegration goodsReturnIntegration) 
+    : IRequestHandler<PostGoodsReturnCmd, bool>
+{
+    public async Task<bool> Handle(PostGoodsReturnCmd request, CancellationToken cancellationToken)
+    {
+        GoodsReturnDTO payload = request.Data.Adapt<GoodsReturnDTO>();
+
+        bool result = await goodsReturnIntegration.PostGoodsReturnAsync(payload);
+
+        return result;
+    }
+}
