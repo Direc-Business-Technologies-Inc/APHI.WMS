a30599b197ea9b0d304fb70ed4208700097b3499
i-nicolas <tbsi.nicolas@gmail.com>
2026-02-24 10:15:49 +0800

    feat: goods issue, goods receipt, & other data dto

---
+++ Application.DataTransferObjects/Transactions/GoodsIssue/GoodsIssueDTO.cs
@@ -0,0 +1,12 @@
+﻿using Application.DataTransferObjects.Others;
+using Application.DataTransferObjects.Transactions.Commons;
+using Application.DataTransferObjects.Transactions.GoodsIssue;
+
+namespace Application.DataTransferObjects.Transactions.Goodsissue;
+
+public class GoodsIssueDTO : MarketingDocumentDTO
+{
+    public string PreparedBy { get; set; }
+    public TransactionTypeDTO TransactionType { get; set; }
+    public IEnumerable<GoodsIssueLineDTO> DocumentLines { get; set; } = [];
+}
---
+++ Application.DataTransferObjects/Transactions/GoodsReceipt/GoodsReceiptDTO.cs
@@ -0,0 +1,11 @@
+﻿using Application.DataTransferObjects.Others;
+using Application.DataTransferObjects.Transactions.Commons;
+
+namespace Application.DataTransferObjects.Transactions.GoodsReceipt;
+
+public class GoodsReceiptDTO : MarketingDocumentDTO
+{
+    public string PreparedBy { get; set; }
+    public TransactionTypeDTO TransactionType { get; set; }
+    public IEnumerable<GoodsReceiptLineDTO> DocumentLines { get; set; } = [];
+}
