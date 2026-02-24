d53038d8bab5fd13d1690bbcdfc3ae7db961b154
i-nicolas <tbsi.nicolas@gmail.com>
2026-02-24 10:01:00 +0800

    feat: goods return UI handler

--- Web.BlazorServer/Handlers/BlazorServerHandlersDI.cs
+++ Web.BlazorServer/Handlers/BlazorServerHandlersDI.cs
@@ -2,12 +2,16 @@
 using Web.BlazorServer.Handlers.Implementations.Administration.Authorization;
 using Web.BlazorServer.Handlers.Implementations.Administration.Role;
 using Web.BlazorServer.Handlers.Implementations.Administration.User;
+using Web.BlazorServer.Handlers.Implementations.Others;
 using Web.BlazorServer.Handlers.Implementations.System;
+using Web.BlazorServer.Handlers.Implementations.Transaction.GoodsReturn;
 using Web.BlazorServer.Handlers.Implementations.Transaction.Receiving;
 using Web.BlazorServer.Handlers.Repositories.Administration.Authorization;
 using Web.BlazorServer.Handlers.Repositories.Administration.Role;
 using Web.BlazorServer.Handlers.Repositories.Administration.User;
+using Web.BlazorServer.Handlers.Repositories.Others;
 using Web.BlazorServer.Handlers.Repositories.System;
+using Web.BlazorServer.Handlers.Repositories.Transaction.GoodsReturn;
 using Web.BlazorServer.Handlers.Repositories.Transaction.Receiving;
 
 namespace Web.BlazorServer.Handlers;
@@ -24,6 +28,10 @@ public static class BlazorServerHandlersDI
         services.TryAddTransient<IDocumentNumberHandler, DocumentNumberHandler>();
         services.TryAddTransient<IAuthorizationHandler, AuthorizationHandler>();
         services.TryAddTransient<IReceivingHandler, ReceivingHandler>();
+        services.TryAddTransient<IGoodsReturnHandler, GoodsReturnHandler>();
+        services.TryAddTransient<IBusinessPartnerHandler, BusinessPartnerHandler>();
+        services.TryAddTransient<IItemMasterDataHandler, ItemMasterDataHandler>();
+        services.TryAddTransient<IWarehouseMasterDataHandler, WarehouseMasterDataHandler>();
 
         return services;
     }
---
+++ Web.BlazorServer/Handlers/Implementations/Others/ItemMasterDataHandler.cs
@@ -0,0 +1,22 @@
+﻿using Application.DataTransferObjects.Transactions.Commons;
+using Application.UseCases.Queries.Others;
+using Mapster;
+using MediatR;
+using Shared.Entities;
+using Web.BlazorServer.Handlers.Repositories.Others;
+using Web.BlazorServer.ViewModels.Transaction.Commons;
+
+namespace Web.BlazorServer.Handlers.Implementations.Others;
+
+public class ItemMasterDataHandler(
+    ISender Sender) 
+    : IItemMasterDataHandler
+{
+    public async Task<(IEnumerable<ItemVM> Data, int Count)> GetMerchandiseItemsAsync(DataGridIntent intent)
+    {
+        GetMerchandiseItemsQry qry = new(intent);
+        (IEnumerable<ItemDTO> Data, int Count) = await Sender.Send(qry);
+
+        return (Data.Adapt<IEnumerable<ItemVM>>(), Count);
+    }
+}
