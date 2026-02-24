5f2901730b82cec0cf27e61ddf1d8efc4f53ba21
i-nicolas <tbsi.nicolas@gmail.com>
2026-02-24 09:52:24 +0800

    feat: goods return payload

---
+++ Integration.SAP/Entities/Transactional/GoodsReturn/PurchaseReturnLinesPayload.cs
@@ -0,0 +1,31 @@
+﻿using Ardalis.GuardClauses;
+
+namespace Integration.SAP.Entities.Transactional.GoodsReturn;
+
+public class PurchaseReturnLinesPayload
+{
+    public int BaseEntry { get; private set; }
+    public int BaseType { get; private set; }
+    public int BaseLine { get; private set; }
+    public int LineNum { get; private set; }
+    public string ItemCode { get; private set; }
+    public decimal Quantity { get; private set; }
+    public string WarehouseCode { get; private set; }
+
+    public PurchaseReturnLinesPayload(int baseEntry,
+                           int baseType,
+                           int baseLine,
+                           int lineNum,
+                           string itemCode,
+                           decimal qty,
+                           string whsCode)
+    {
+        BaseEntry = Guard.Against.Negative(baseEntry, nameof(BaseEntry), "Base Entry cannot be negative or null");
+        BaseType = Guard.Against.Negative(baseType, nameof(BaseType), "Base Type cannot be negative or null");
+        BaseLine = Guard.Against.Negative(baseLine - 1, nameof(BaseLine), "Base Line cannot be negative or null");
+        LineNum = Guard.Against.Negative(lineNum, nameof(LineNum), "Base Line cannot be negative or null");
+        ItemCode = Guard.Against.NullOrEmpty(itemCode, nameof(ItemCode), "Item Code cannot be null or empty");
+        Quantity = Guard.Against.NegativeOrZero(qty, nameof(Quantity), "Quantity cannot be negative or zero");
+        WarehouseCode = Guard.Against.NullOrEmpty(whsCode, nameof(WarehouseCode), "Warehouse Code cannot be null or empty");
+    }
+}
---
+++ Integration.SAP/Entities/Transactional/GoodsReturn/PurchaseReturnPayload.cs
@@ -0,0 +1,30 @@
+﻿using Ardalis.GuardClauses;
+
+namespace Integration.SAP.Entities.Transactional.GoodsReturn;
+
+public class PurchaseReturnPayload
+{
+    public DateTime DocDate { get; private set; }
+    public DateTime DocDueDate { get; private set; }
+    public string CardCode { get; private set; }
+    public string U_PrepBy { get; private set; }
+    public string Comments { get; private set; }
+
+    public List<PurchaseReturnLinesPayload> DocumentLines { get; private set; } = [];
+
+    public PurchaseReturnPayload(DateTime docDate,
+                       DateTime docDueDate,
+                       string cardCode,
+                       string prepBy,
+                       string comments,
+                       List<PurchaseReturnLinesPayload> documentLines)
+    {
+        DocDate = Guard.Against.NullOrOutOfSQLDateRange(docDate, nameof(DocDate), "Doc Date cannot be null or out of range");
+        DocDueDate = Guard.Against.NullOrOutOfSQLDateRange(docDueDate, nameof(DocDueDate), "Doc Due Date cannot be null or out of range");
+        CardCode = Guard.Against.NullOrEmpty(cardCode, nameof(CardCode), "Card Code cannot be null or empty");
+        U_PrepBy = Guard.Against.NullOrEmpty(prepBy, nameof(U_PrepBy), "Prepared By cannot be null or empty");
+        Comments = "Posted from WMS";
+
+        DocumentLines = [.. Guard.Against.NullOrEmpty(documentLines, nameof(DocumentLines), "Document Lines cannot be null or empty")];
+    }
+}
