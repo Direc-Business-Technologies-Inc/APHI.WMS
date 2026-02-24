964a24fb148798826695db1bd1b36113062f30be
i-nicolas <tbsi.nicolas@gmail.com>
2026-02-24 09:12:22 +0800

    feat: goods return sap dto

---
+++ Application.DataTransferObjects/Transactions/GoodsReturn/SAP/GoodsReturnLineSAPDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.DataTransferObjects.Transactions.GoodsReturn.SAP;
+
+public class GoodsReturnLineSAPDTO
+{
+    public int DocEntry { get; set; }
+    public int DocNum { get; set; }
+    public string LineNum { get; set; }
+    public string ItemCode { get; set; }
+    public string ItemName { get; set; }
+    public decimal Quantity { get; set; }
+    public string UoMCode { get; set; }
+    public decimal UoMValue { get; set; }
+    public string UoMName { get; set; }
+    public string WhsCode { get; set; }
+    public string WhsName { get; set; }
+}
