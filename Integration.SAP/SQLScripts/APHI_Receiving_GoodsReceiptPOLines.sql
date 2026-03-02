1d1f1f730e326fb6b39529d76b547417ee8708b8
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-02 10:30:30 +0800

    fix: il_id_022
    - fixed qry to get datagrid details

--- Integration.SAP/SQLScripts/APHI_Receiving_GoodsReceiptPOLines.sql
+++ Integration.SAP/SQLScripts/APHI_Receiving_GoodsReceiptPOLines.sql
@@ -2,6 +2,8 @@ SELECT
 	 T0.DocEntry
 	,T0.DocNum
 	,T1.LineNum
+	,T3.WhsCode
+	,T3.WhsName
 	,T1.ItemCode
 	,T2.ItemName
 	,T1.Quantity
@@ -12,6 +14,7 @@ SELECT
 FROM OPDN AS T0
 INNER JOIN PDN1 AS T1 ON T0.DocEntry = T1.DocEntry
 INNER JOIN OITM AS T2 ON T1.ItemCode = T2.ItemCode
+INNER JOIN OWHS AS T3 ON T1.WhsCode = T3.WhsCode
 LEFT JOIN OUOM AS T4 ON T1.UomEntry = T4.UomEntry
 WHERE
 	T0.DocEntry = @DocEntry
