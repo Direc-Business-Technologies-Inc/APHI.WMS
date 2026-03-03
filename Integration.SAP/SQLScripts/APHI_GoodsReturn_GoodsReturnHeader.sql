fa22af56dba4ff070c334210bc68d1ce9016340a
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-03 20:19:27 +0800

    feat: goods return & goods return request sql qry addtl fields

--- Integration.SAP/SQLScripts/APHI_GoodsReturn_GoodsReturnHeader.sql
+++ Integration.SAP/SQLScripts/APHI_GoodsReturn_GoodsReturnHeader.sql
@@ -17,6 +17,17 @@ SELECT
 		WHEN T0.DocStatus = 'C' THEN 'Closed'
 	 ELSE 'Open'
 	 END as DocStatus
+	,T0.U_SchlYear [SchoolYear]
+	,T0.U_RetType [ReturnType]
+	,T0.U_DRNo
+	,T0.U_SINo
+	,T0.U_DelBy [DeliveredBy]
+	,T0.U_RecBy [ReceivedBy]
+	,T0.U_Remarks [DocRemarks]
+	,T0.U_PrepBy [PreparedBy]
+	,T0.U_RevBy [ReviewedBy]
+	,T0.U_AppBy [ApprovedBy]
+	,T0.U_CheckBy [CheckedBy]
 FROM ORPD T0
 INNER JOIN RPD1 T1 ON T1.DocEntry = T0.DocEntry
 LEFT JOIN PDN1 T2 ON T2.DocEntry = T1.BaseEntry AND T2.ObjType = T1.BaseType
