807973ee8a2b0e1789ea065413bc9995e442dcd8
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-03 16:37:37 +0800

    fix: adjusted goods issue DTO to new fields

--- Application.DataTransferObjects/Transactions/GoodsIssue/GoodsIssueDTO.cs
+++ Application.DataTransferObjects/Transactions/GoodsIssue/GoodsIssueDTO.cs
@@ -1,17 +1,20 @@
 ﻿using Application.DataTransferObjects.Others;
 using Application.DataTransferObjects.Transactions.Commons;
 using Application.DataTransferObjects.Transactions.GoodsIssue;
+using Domain.Entities.ValueObjects.Others;
 
 namespace Application.DataTransferObjects.Transactions.Goodsissue;
 
 public class GoodsIssueDTO : MarketingDocumentDTO
 {
-    public string PreparedBy { get; set; }
+    public string? SchoolYear { get; set; }
     public TransactionTypeDTO TransactionType { get; set; }
-    public string SchoolYear { get; set; }
-    public string SrfNo { get; set; }
-    public string Designation { get; set; }
-    public string DocRemarks { get; set; }
-    public string ApprovedBy { get; set; }
+    public string? SrfNo { get; set; }
+    public string? Designation { get; set; }
+    public string? ReceivedBy { get; set; }
+    public string? DocRemarks { get; set; }
+    public string PreparedBy { get; set; }
+    public string? ApprovedBy { get; set; }
+    public string? NotedBy { get; set; }
     public IEnumerable<GoodsIssueLineDTO> DocumentLines { get; set; } = [];
 }
--- Application.DataTransferObjects/Transactions/GoodsIssue/SAP/GoodsIssueHeaderSAPDTO.cs
+++ Application.DataTransferObjects/Transactions/GoodsIssue/SAP/GoodsIssueHeaderSAPDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace Application.DataTransferObjects.Transactions.GoodsIssue;
+﻿using Application.DataTransferObjects.Others;
+
+namespace Application.DataTransferObjects.Transactions.GoodsIssue;
 
 public class GoodsIssueHeaderSAPDTO
 {
@@ -12,7 +14,9 @@ public class GoodsIssueHeaderSAPDTO
     public string AcctName { get; set; }
     public string SchoolYear { get; set; }
     public string SrfNo { get; set; }
-    public string Designation { get; set; }
     public string DocRemarks { get; set; }
+    public string Designation { get; set; }
+    public string ReceivedBy { get; set; }
     public string ApprovedBy { get; set; }
+    public string NotedBy { get; set; }
 }
