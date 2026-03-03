1368fc9ebec9a9dc1ab931f019d504098e3ede32
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-03 18:42:22 +0800

    feat: adjusted goods receipt sql qry for addtl fields

--- Integration.SAP/SQLScripts/APHI_GoodsReceipt_Header.sql
+++ Integration.SAP/SQLScripts/APHI_GoodsReceipt_Header.sql
@@ -2,11 +2,20 @@ SELECT
        T0.DocEntry
      , T0.DocNum
      , T0.DocDate
-     , T0.U_PrepBy [PreparedBy]
      , T1.Code [TransTypeCode]
      , T1.Name [TransTypeName]
      , T2.AcctCode
      , T2.AcctName
+     , T0.U_BpCode [CardCode]
+     , T0.U_BPName [CardName]
+     , T0.U_WARNo [WarNo]
+     , T0.U_PURNo [PurNo]
+     , T0.U_AppBy [ApprovedBy]
+     , T0.U_Desig [Designation]
+     , T0.U_RecBy [ReceivedBy]
+     , T0.U_Remarks [DocRemarks]
+     , T0.U_PrepBy [PreparedBy]
+     , T0.U_NotedBy [NotedBy]
 FROM OIGN AS T0
 LEFT JOIN [@TRANSACTION_TYPE] T1 ON T1.Code = T0.U_TransType
 LEFT JOIN OACT T2 ON T2.FormatCode = T1.U_AcctCode
