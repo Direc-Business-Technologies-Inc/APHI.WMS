b00103b2db835b5587c6542eb4cd9d47328531ed
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-03 16:15:10 +0800

    fix: adjusted Goods Issue Header queries

--- Integration.SAP/SQLScripts/APHI_GoodsIssue_Header.sql
+++ Integration.SAP/SQLScripts/APHI_GoodsIssue_Header.sql
@@ -7,6 +7,11 @@ SELECT
      , T1.Name [TransTypeName]
      , T2.AcctCode
      , T2.AcctName
+     , T0.U_SchlYear [SchoolYear]
+     , T0.U_SRFNo [SrfNo]
+     , T0.U_Desig [Designation]
+     , T0.U_Remarks [DocRemarks]
+     , T0.U_AppBy [ApprovedBy]
 FROM OIGE AS T0
 LEFT JOIN [@TRANSACTION_TYPE] T1 ON T1.Code = T0.U_TransType
 LEFT JOIN OACT T2 ON T2.FormatCode = T1.U_AcctCode
