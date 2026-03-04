b4cd75d9475ca8e5540fe7b70d86b7d438eb10b5
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-04 19:23:59 +0800

    feat: receiving addtl fields

--- Application.DataTransferObjects/Transactions/Receiving/SAP/PurchaseDeliveryNoteHeaderSAPDTO.cs
+++ Application.DataTransferObjects/Transactions/Receiving/SAP/PurchaseDeliveryNoteHeaderSAPDTO.cs
@@ -11,5 +11,18 @@ public class PurchaseDeliveryNoteHeaderSAPDTO
     public string CardCode { get; set; }
     public string CardName { get; set; }
     public string SupplierContactPerson { get; set; }
-    public string ReceivedBy { get; set; }
+    public string PreparedBy { get; set; }
+    public string? PONo { get; set; }
+    public string? DRNo { get; set; }
+    public string? Designation { get; set; }
+    public string? ReceivedBy { get; set; }
+    public string? ApprovedBy { get; set; }
+    public string? NotedBy { get; set; }
+    public string? SchoolYear { get; set; }
+    public string? SINo { get; set; }
+    public string? DeliveredBy { get; set; }
+    public string? ReviewedBy { get; set; }
+    public string? PurchaseType { get; set; }
+    public string? ItemName { get; set; }
+    public string? DocRemarks { get; set; }
 }
