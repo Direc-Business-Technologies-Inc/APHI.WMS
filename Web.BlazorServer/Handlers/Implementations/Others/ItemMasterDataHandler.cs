44d77897adb865a13611b3e7860237fdc39202ce
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-04 21:20:53 +0800

    feat: item in whs item master data handler

--- Web.BlazorServer/Handlers/Implementations/Others/ItemMasterDataHandler.cs
+++ Web.BlazorServer/Handlers/Implementations/Others/ItemMasterDataHandler.cs
@@ -9,14 +9,30 @@ using Web.BlazorServer.ViewModels.Transaction.Commons;
 namespace Web.BlazorServer.Handlers.Implementations.Others;
 
 public class ItemMasterDataHandler(
-    ISender Sender) 
+    ISender Sender)
     : IItemMasterDataHandler
 {
+    public async Task<(IEnumerable<ItemVM> Data, int Count)> GetItemsInWarehouseAsync(DataGridIntent intent, string whsCode, List<string> itemCodes)
+    {
+        GetItemsInWarehouseQry qry = new(intent, whsCode, itemCodes);
+        (IEnumerable<ItemDTO> Data, int Count) response = await Sender.Send(qry);
+
+        return (response.Data.Adapt<IEnumerable<ItemVM>>(), response.Count);
+    }
+
     public async Task<(IEnumerable<ItemVM> Data, int Count)> GetMerchandiseItemsAsync(DataGridIntent intent)
     {
         GetMerchandiseItemsQry qry = new(intent);
         (IEnumerable<ItemDTO> Data, int Count) = await Sender.Send(qry);
 
         return (Data.Adapt<IEnumerable<ItemVM>>(), Count);
+    }
+
+    public async Task<(IEnumerable<ItemVM> Data, int Count)> GetWarehouseItemsAsync(DataGridIntent intent, string whsCode)
+    {
+        GetAllWarehouseItemsQry qry = new(intent, whsCode);
+        (IEnumerable<ItemDTO> Data, int Count) response = await Sender.Send(qry);
+
+        return (response.Data.Adapt<IEnumerable<ItemVM>>(), response.Count);
     }
 }
--- Web.BlazorServer/Handlers/Repositories/Others/IItemMasterDataHandler.cs
+++ Web.BlazorServer/Handlers/Repositories/Others/IItemMasterDataHandler.cs
@@ -6,4 +6,6 @@ namespace Web.BlazorServer.Handlers.Repositories.Others;
 public interface IItemMasterDataHandler
 {
     Task<(IEnumerable<ItemVM> Data, int Count)> GetMerchandiseItemsAsync(DataGridIntent intent);
+    Task<(IEnumerable<ItemVM> Data, int Count)> GetWarehouseItemsAsync(DataGridIntent intent, string whsCode);
+    Task<(IEnumerable<ItemVM> Data, int Count)> GetItemsInWarehouseAsync(DataGridIntent intent, string whsCode, List<string> itemCodes);
 }
