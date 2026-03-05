812159344986e391eabb807b1adff1678dc230a9
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-05 11:01:39 +0800

    fix: adjusted of GRPO based posting

--- Integration.SAP/Entities/Transactional/GoodsReturn/PurchaseReturnLinesPayload.cs
+++ Integration.SAP/Entities/Transactional/GoodsReturn/PurchaseReturnLinesPayload.cs
@@ -20,9 +20,9 @@ public class PurchaseReturnLinesPayload
                            decimal qty,
                            string whsCode)
     {
-        BaseEntry = Guard.Against.Negative(baseEntry, nameof(BaseEntry), "Base Entry cannot be negative or null");
+        BaseEntry = Guard.Against.Null(baseEntry, nameof(BaseEntry), "Base Entry cannot be negative or null");
         BaseType = Guard.Against.Null(baseType, nameof(BaseType), "Base Type cannot be null");
-        BaseLine = Guard.Against.Negative(baseLine - 1, nameof(BaseLine), "Base Line cannot be negative or null");
+        BaseLine = Guard.Against.Null(baseLine == -1 ? baseLine : baseLine - 1, nameof(BaseLine), "Base Line cannot be negative or null");
         LineNum = Guard.Against.Negative(lineNum, nameof(LineNum), "Base Line cannot be negative or null");
         ItemCode = Guard.Against.NullOrEmpty(itemCode, nameof(ItemCode), "Item Code cannot be null or empty");
         Quantity = Guard.Against.NegativeOrZero(qty, nameof(Quantity), "Quantity cannot be negative or zero");
