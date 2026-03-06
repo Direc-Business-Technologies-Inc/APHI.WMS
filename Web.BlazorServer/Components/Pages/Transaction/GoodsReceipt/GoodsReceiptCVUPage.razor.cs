9c705c7a08f7e1d78dbf79db4cfb685bdd9c5841
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-06 10:43:10 +0800

    fix: goods receipt 0 qty blockings

--- Web.BlazorServer/Components/Pages/Transaction/GoodsReceipt/GoodsReceiptCVUPage.razor.cs
+++ Web.BlazorServer/Components/Pages/Transaction/GoodsReceipt/GoodsReceiptCVUPage.razor.cs
@@ -117,6 +117,12 @@ public partial class GoodsReceiptCVUPage
             return;
         }
 
+        if (FormData.DocumentLines.All(x => x.Quantity <= 0))
+        {
+            ToastService.Warning("Please provide Quantities to the selected Items");
+            return;
+        }
+
         if (FormData.DocumentLines.Any(x => x.Quantity <= 0))
         {
             if (!await AlertService.PromptAsync("Some Items in the Goods Receipt has no Quantity. These Items will be removed in the transaction. Are you sure wou want to proceed?"))
