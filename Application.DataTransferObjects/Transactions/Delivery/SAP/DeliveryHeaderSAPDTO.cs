ad73e2b71d9c2a8ee9130651790c732554f5206e
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-10 16:48:04 +0800

    fix: delivery header sap DTO naming

--- Application.DataTransferObjects/Transactions/Delivery/SAP/DeliveryHeaderSAPDTO.cs
+++ Application.DataTransferObjects/Transactions/Delivery/SAP/DeliveryHeaderSAPDTO.cs
@@ -2,41 +2,29 @@
 
 internal class DeliveryHeaderSAPDTO
 {
-
-    public string CardCode { get; set; }
-    public string CardName { get; set; }
-    public string CntctCode { get; set; }
-    public int NumAtCard { get; set; }
-
     public int DocNum { get; set; }
     public int DocEntry { get; set; }
     public DateTime DocDate { get; set; }
     public DateTime DocDueDate { get; set; }
-    public DateTime TaxDate { get; set; }
+    
+    public string CardCode { get; set; }
+    public string CardName { get; set; }
+    public string ContactPerson { get; set; }
+    public int NumAtCard { get; set; }
 
-    public string ItemCode { get; set; }
-    public string Dscription { get; set; }
-    public int Quantity { get; set; }
-    public string UoMCode { get; set; }
-    public string WhsCode { get; set; }
-    public string WhsName { get; set; }
-
-    public string SchlYear { get; set; }
-    public string DRNo { get; set; }
-    public string ActualDelivDate { get; set; }
-    public string DelivMeans { get; set; }
-    public string Courier { get; set; }
-    public string CourName { get; set; }
-    public string Desig { get; set; }
-    public string WBNo { get; set; }
-    public string PlateNo { get; set; }
-    public string Driver { get; set; }
-    public string Remarks { get; set; }
-    public string RecBy { get; set; }
-    public string PrepBy { get; set; }
-    public string AppBy { get; set; }
-    public string NotedBy { get; set; }
-
-
-
+    public string? SchoolYear { get; set; }
+    public string? DRNo { get; set; }
+    public string? ActualDeliveryDate { get; set; }
+    public string? DeliveryMeans { get; set; }
+    public string? Courier { get; set; }
+    public string? CourierName { get; set; }
+    public string? Designation { get; set; }
+    public string? WaybillNo { get; set; }
+    public string? PlateNo { get; set; }
+    public string? Driver { get; set; }
+    public string? DocRemarks { get; set; }
+    public string? ReceivedBy { get; set; }
+    public string? PreparedBy { get; set; }
+    public string? ApprovedBy { get; set; }
+    public string? NotedBy { get; set; }
 }
