7af886519e6ba8551f9c4f501f06700ddb7fbe2e
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-10 16:55:25 +0800

    fix: delivery DTO property names

--- Application.DataTransferObjects/Transactions/Delivery/DeliveryDTO.cs
+++ Application.DataTransferObjects/Transactions/Delivery/DeliveryDTO.cs
@@ -1,33 +1,27 @@
 ﻿using Application.DataTransferObjects.Others;
+using Application.DataTransferObjects.Transactions.Commons;
 using Application.DataTransferObjects.Transactions.GoodsReceipt;
 
 namespace Application.DataTransferObjects.Transactions.Delivery;
 
-internal class DeliveryDTO
+internal class DeliveryDTO : MarketingDocumentDTO
 {
-   
-    public string? DocRemarks { get; set; } = null;
-    public string? Designation { get; set; } = null;
-    public string? ReceivedBy { get; set; } = null;
-    public string? ApprovedBy { get; set; } = null;
-    public string? NotedBy { get; set; } = null;
-    public string? Customer { get; set; } = null;
-    public string? Name { get; set; } = null;
-    public string? ContactPerson { get; set; } = null;
-    public string? DRNo { get; set; } = null;
-    public int DocNum { get; set; }
-
     public DateTime PostingDate { get; set; }
     public DateTime DeliveryDate { get; set; }
     public DateTime DocumentDate { get; set; }
+
+    public string? ContactPerson { get; set; } = null;
     public string? SchoolYear { get; set; } = null;
     public string? ActualDelivDate { get; set; } = null;
     public string? CourierName { get; set; } = null;
     public string? WayBillNo { get; set; } = null;
     public string? PlateNo { get; set; } = null;
-
+    public string? DRNo { get; set; } = null;
+    public string? DocRemarks { get; set; } = null;
+    public string? Designation { get; set; } = null;
+    public string? ReceivedBy { get; set; } = null;
+    public string? ApprovedBy { get; set; } = null;
+    public string? NotedBy { get; set; } = null;
 
     public IEnumerable<DeliveryLineDTO> DocumentLines { get; set; } = [];
-
-
 }
