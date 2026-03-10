faf240f2452ed921b9fbff3883eba0a9dfb29f7a
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-10 14:30:54 +0800

    feat: Sales Order SAP DTO

---
+++ Application.DataTransferObjects/Transactions/Delivery/SAP/SalesOrderHeaderSAPDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.DataTransferObjects.Transactions.Delivery.SAP;
+
+public class SalesOrderHeaderSAPDTO
+{
+    public int DocEntry { get; set; }
+    public int DocNum { get; set; }
+    public DateTime DocDate { get; set; }
+    public string CardCode { get; set; }
+    public string CardName { get; set; }
+    public string ContactPerson { get; set; }
+    public string DocRemarks { get; set; }
+    public string PrepBy { get; set; }
+    public string RevBy { get; set; }
+}
---
+++ Application.DataTransferObjects/Transactions/Delivery/SAP/SalesOrderLinesSAPDTO.cs
@@ -0,0 +1,11 @@
+﻿using Application.DataTransferObjects.Transactions.Commons;
+
+namespace Application.DataTransferObjects.Transactions.Delivery.SAP;
+
+public class SalesOrderLinesSAPDTO : ItemDTO
+{
+    public string WhsCode { get; set; }
+    public string WhsName { get; set; }
+    public string TargetQty { get; set; }
+    public string OpenQty { get; set; }
+}
