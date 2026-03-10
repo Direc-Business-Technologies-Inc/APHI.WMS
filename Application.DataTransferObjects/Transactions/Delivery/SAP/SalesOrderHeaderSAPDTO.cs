7ecfb63eb2e7f27815b5283ec282a1fc1a4cf0b8
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-10 16:48:38 +0800

    feat: re-adjusted SO SAP header

--- Application.DataTransferObjects/Transactions/Delivery/SAP/SalesOrderHeaderSAPDTO.cs
+++ Application.DataTransferObjects/Transactions/Delivery/SAP/SalesOrderHeaderSAPDTO.cs
@@ -6,24 +6,19 @@ public class SalesOrderHeaderSAPDTO
     public int DocNum { get; set; }
     public DateTime DocDate { get; set; }
     public DateTime DocDueDate { get; set; }
-    public DateTime TaxDate { get; set; }
     public string CardCode { get; set; }
     public string CardName { get; set; }
-    public string CntctCode { get; set; }
+    public string ContactPerson { get; set; }
     public string NumAtCard { get; set; }
 
-    public string SchlYear { get; set; }
-    public string PONo { get; set; }
-    public string Area { get; set; }
-    public string Desig { get; set; }
-    public string OrdBy { get; set; }
-    public string Remarks { get; set; }
-    public string DocRemarks { get; set; }
-    public string PrepBy { get; set; }
-    public string RevBy { get; set; }
-    public string AppBy { get; set; }
-    public string NotedBy { get; set; }
-
-
-
+    public string? SchoolYear { get; set; }
+    public string? PONo { get; set; }
+    public string? Area { get; set; }
+    public string? Designation { get; set; }
+    public string? OrderBy { get; set; }
+    public string? DocRemarks { get; set; }
+    public string? PreparedBy { get; set; }
+    public string? ReviewedBy { get; set; }
+    public string? AppprovedBy { get; set; }
+    public string? NotedBy { get; set; }
 }
