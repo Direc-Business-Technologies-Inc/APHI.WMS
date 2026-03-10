26617f03e1301407fc6f0712cc55efb1a9f76934
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-10 16:56:00 +0800

    feat: sales order sql qries

---
+++ Integration.SAP/SQLScripts/APHI_SalesOrder_DataGrid.sql
@@ -0,0 +1,13 @@
+﻿SELECT
+	ORDR.DocEntry
+	,ORDR.DocNum
+	,ORDR.DocDate
+	,ORDR.CardCode
+	,OCRD.CardName
+	,OCRD.CntctPrsn
+	,ORDR.U_Remarks [Remarks]
+FROM ORDR
+INNER JOIN OCRD ON OCRD.CardCode = ORDR.CardCode
+WHERE 
+	ORDR.DocStatus = 'O'
+	AND ORDR.CANCELED = 'N'
