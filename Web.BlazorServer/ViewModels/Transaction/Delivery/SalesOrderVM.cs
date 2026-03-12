c99e445b6e018c89421366b034a2c5a8acfb1617
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-12 17:41:25 +0800

    feat: sales order VM

--- Web.BlazorServer/ViewModels/Transaction/Delivery/SalesOrderVM.cs
+++ Web.BlazorServer/ViewModels/Transaction/Delivery/SalesOrderVM.cs
@@ -1,6 +1,20 @@
-﻿namespace Web.BlazorServer.ViewModels.Transaction.Delivery;
+using Web.BlazorServer.ViewModels.Transaction.Commons;
 
-public class SalesOrderVM
+namespace Web.BlazorServer.ViewModels.Transaction.Delivery;
+
+public class SalesOrderVM : MarketingDocumentVM
 {
-
+    public string? ContactPerson { get; set; }
+    public string? DRNo { get; set; }
+    public string? SchoolYear { get; set; }
+    public string? PONo { get; set; }
+    public string? Area { get; set; }
+    public string? Designation { get; set; }
+    public string? OrderedBy { get; set; }
+    public string? DocRemarks { get; set; }
+    public string? PreparedBy { get; set; }
+    public string? ReviewedBy { get; set; }
+    public string? ApprovedBy { get; set; }
+    public string? NotedBy { get; set; }
+    public IEnumerable<SalesOrderLineVM> DocumentLines { get; set; } = [];
 }
