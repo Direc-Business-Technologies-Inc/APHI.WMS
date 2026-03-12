ddd057380c7231e72e4e962a060c9b3dc8abd18b
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-12 15:21:14 +0800

    feat: included target qty and open qty on SO Lines

--- Application.DataTransferObjects/Transactions/Delivery/SalesOrderLineDTO.cs
+++ Application.DataTransferObjects/Transactions/Delivery/SalesOrderLineDTO.cs
@@ -6,5 +6,6 @@ namespace Application.DataTransferObjects.Transactions.Delivery;
 public class SalesOrderLineDTO:ItemDTO
 {
     public WarehouseDTO Warehouse { get; set; }
-
+    public decimal TargetQty { get; set; }
+    public decimal OpenQty { get; set; }
 }
