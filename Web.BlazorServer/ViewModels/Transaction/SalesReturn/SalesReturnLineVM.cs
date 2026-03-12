c548f3406d97729a8d114d71828320f38352781b
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-12 20:59:41 +0800

    feat: sales return VMs

---
+++ Web.BlazorServer/ViewModels/Transaction/SalesReturn/SalesReturnLineVM.cs
@@ -0,0 +1,9 @@
+﻿using Web.BlazorServer.ViewModels.Transaction.Commons;
+
+namespace Web.BlazorServer.ViewModels.Transaction.SalesReturn;
+
+public class SalesReturnLineVM : ItemVM
+{
+    public int DocEntry { get; set; }
+    public int DocNum { get; set; }
+}
---
+++ Web.BlazorServer/ViewModels/Transaction/SalesReturn/SalesReturnRequestLineVM.cs
@@ -0,0 +1,11 @@
+﻿using Web.BlazorServer.ViewModels.Transaction.Commons;
+
+namespace Web.BlazorServer.ViewModels.Transaction.SalesReturn;
+
+public class SalesReturnRequestLineVM : ItemVM
+{
+    public int DocEntry { get; set; }
+    public int DocNum { get; set; }
+    public decimal TargetQuantity { get; set; }
+    public decimal OpenQuantity { get; set; }
+}
