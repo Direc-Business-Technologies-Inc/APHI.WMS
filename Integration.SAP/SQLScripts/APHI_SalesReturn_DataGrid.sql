6df4cbcfa48acfdaa0111da0262b6443eb75f925
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-12 20:58:41 +0800

    feat: sales return sql queries

---
+++ Integration.SAP/SQLScripts/APHI_SalesReturn_DataGrid.sql
@@ -0,0 +1,11 @@
+SELECT
+	 ORDN.DocDate
+	,ORDN.DocEntry
+	,ORDN.DocNum
+	,OCRD.CardCode
+	,OCRD.CardName
+	,ISNULL(ORDN.U_Remarks, '-') [Remarks]
+FROM ORDN
+INNER JOIN OCRD ON OCRD.CardCode = ORDN.CardCode
+WHERE ORDN.Comments LIKE '%WMS%'
+ORDER BY ORDN.DocDate DESC
