32c456b19f4fe2b77d89938b9518a934f7e54f7e
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-12 20:58:31 +0800

    feat: sales return DTO

---
+++ Application.DataTransferObjects/Transactions/SalesReturn/SalesReturnLineDTO.cs
@@ -0,0 +1,9 @@
+﻿using Application.DataTransferObjects.Transactions.Commons;
+
+namespace Application.DataTransferObjects.Transactions.SalesReturn;
+
+public class SalesReturnLineDTO : ItemDTO
+{
+    public int DocEntry { get; set; }
+    public int DocNum { get; set; }
+}
---
+++ Application.DataTransferObjects/Transactions/SalesReturn/SalesReturnRequestLineDTO.cs
@@ -0,0 +1,11 @@
+﻿using Application.DataTransferObjects.Transactions.Commons;
+
+namespace Application.DataTransferObjects.Transactions.SalesReturn;
+
+public class SalesReturnRequestLineDTO : ItemDTO
+{
+    public int DocEntry { get; set; }
+    public int DocNum { get; set; }
+    public decimal TargetQuantity { get; set; }
+    public decimal OpenQuantity { get; set; }
+}
