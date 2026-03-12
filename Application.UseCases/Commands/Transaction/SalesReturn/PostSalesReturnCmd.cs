eb60e098b400588c31548f271e5e325aaadfbc49
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-12 20:59:16 +0800

    feat: sales return commands

---
+++ Application.UseCases/Commands/Transaction/SalesReturn/PostSalesReturnCmd.cs
@@ -0,0 +1,16 @@
+﻿using Application.DataTransferObjects.Transactions.SalesReturn;
+using Application.UseCases.Repositories.Bases;
+using MediatR;
+
+namespace Application.UseCases.Commands.Transaction.SalesReturn;
+
+public record PostSalesReturnCmd(SalesReturnDTO Data) : ITransactionalRequest<bool>;
+
+public class PostSalesReturnCmdHandler() : IRequestHandler<PostSalesReturnCmd, bool>
+{
+    public Task<bool> Handle(PostSalesReturnCmd request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
+
