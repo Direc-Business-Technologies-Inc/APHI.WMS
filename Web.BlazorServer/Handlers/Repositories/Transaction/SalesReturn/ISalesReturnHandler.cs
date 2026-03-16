c0d2d8553ed99676789dbb85f77b30eb78ac55a3
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-03-16 11:32:09 +0800

    feat: Sales Return & BP handlers

--- Web.BlazorServer/Handlers/Repositories/Transaction/SalesReturn/ISalesReturnHandler.cs
+++ Web.BlazorServer/Handlers/Repositories/Transaction/SalesReturn/ISalesReturnHandler.cs
@@ -1,4 +1,5 @@
-﻿using Shared.Entities;
+using Domain.Entities.Enums.Transaction.SalesReturn;
+using Shared.Entities;
 using Web.BlazorServer.ViewModels.Transaction.SalesReturn;
 
 namespace Web.BlazorServer.Handlers.Repositories.Transaction.SalesReturn;
@@ -9,6 +10,7 @@ public interface ISalesReturnHandler
     Task<SalesReturnVM?> GetSalesReturnAsync(int docEntry);
     Task<(IEnumerable<SalesReturnRequestDataGridVM> Data, int Count)> GetSalesReturnRequestDataGridAsync(DataGridIntent intent);
     Task<SalesReturnRequestVM?> GetSalesReturnRequestAsync(int docEntry);
-    Task<IEnumerable<ReturnTypeVM>> GetReturnTypesAsync(int docEntry);
-    Task<bool> PostSalesReturnAsync(SalesReturnVM data);
+    Task<IEnumerable<ReturnTypeVM>> GetReturnTypesAsync();
+    Task<bool> PostSalesReturnAsync(SalesReturnVM data, SalesReturnPostingSource source);
+    Task<bool> PostSalesReturnFromRequestAsync(SalesReturnRequestVM data);
 }
