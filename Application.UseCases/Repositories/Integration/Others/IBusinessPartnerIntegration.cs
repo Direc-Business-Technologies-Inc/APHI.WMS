c9e9dc7158505510685426695ff22f104d70601f
tbsi-iannicolas <tbsi.nicolas@gmail.com>
2026-03-31 17:00:11 +0800

    [FEAT] Sales Return (#7)
    * feat: Sales Order SAP DTO
    
    * feat: Delivery Means DTO
    
    * Squash merge intern/feat/delivery into feat/delivery
    
    * fix: delivery header sap DTO naming
    
    * feat: re-adjusted SO SAP header
    
    * feat: delivery datagrid sql qry
    
    * feat: delivery datagrid sql qry
    
    * feat: delivery header
    
    * feat: delivery lines
    
    * fix: delivery DTO property names
    
    * feat: sales order sql qries
    
    * feat: delivery DTO
    
    * feat: sales order DTO
    
    * feat: delivery sap dto
    
    * feat: sales order line sap dto
    
    * feat: IDeliveryIntegration implementation
    
    * feat: get delivery datagrid qry
    
    * feat: get delivery means qry
    
    * feat: get delivery qry
    
    * fix: rename DocumentLines property
    
    * feat: get sales order queries
    
    * feat: added post action to integration
    
    * feat: delivery notes payload
    
    * feat: claude integration
    
    * feat: included target qty and open qty on SO Lines
    
    * feat: post delivery cmd
    
    * feat: delivery notes payload
    
    * feat: updated queries for sales order
    
    * feat: delivery and sales order VM
    
    * feat: delivery handlers (unimplemented)
    
    * Squash merge feat/delivery into claude/initialize
    
    * feat: ignore claude local files
    
    * feat: delivery integration posting
    
    * feat: delivery handler implementation
    
    * feat: sales order VM
    
    * feat: delivery handler adjustments
    
    * feat: delivery app actions
    
    * feat: sales order datagrid
    
    * feat: delivery data grid
    
    * feat: delivery module page
    
    * feat: sales order cvu page
    
    * feat: delivery cvu page
    
    * feat: new ui abstractions rules
    
    * feat: delivery DTO added numAtCard
    
    * feat: delivery DTO and SAPDTO mapping
    
    * feat: delivery notes udfs
    
    * feat: added JSON string checking
    
    * feat: delivery sql query adjustments
    
    * feat: num at card on delivery VM
    
    * feat: UI adjustments
    
    * feat: sales return SAP DTO
    
    * feat: sales return DTO
    
    * feat: sales return sql queries
    
    * feat: mapper registration of Sales return DTOs
    
    * feat: sales return integration
    
    * feat: sales return commands
    
    * feat: sales return queries
    
    * feat: sales return handlers
    
    * feat: sales return VMs
    
    * feat: gitignore added new claude exceptions
    
    * feat: sales return posting src enum
    
    * feat: sales return DTOs
    
    * feat: IBusinessPartner integration
    
    * feat: ISalesReturn integration adjustments
    
    * feat: mapping profile for sales return
    
    * feat: sales return payload
    
    * feat: integrations
    
    * feat: APHI sales return
    
    * feat: get customer qry
    
    * feat: gafeat: get sales return qry
    
    * feat: posting cmd
    
    * feat: sales return app actions masterdata
    
    * feat: sales return VMs
    
    * feat: Sales Return & BP handlers
    
    * feat: Sales Return & BP handlers
    
    * feat: sales return datagrids
    
    * feat: sales return item selection
    
    * feat: sales return grid
    
    * sales: sales return deliery selection
    
    * feat: sales return page
    
    * feat: sales return CVU page
    
    * feat: version management

--- Application.UseCases/Repositories/Integration/Others/IBusinessPartnerIntegration.cs
+++ Application.UseCases/Repositories/Integration/Others/IBusinessPartnerIntegration.cs
@@ -5,6 +5,7 @@ namespace Application.UseCases.Repositories.Integration.Others;
 
 public interface IBusinessPartnerIntegration
 {
+    Task<(IEnumerable<BusinessPartnerSAPDTO> Data, int Count)> GetCustomersAsync(DataGridIntent intent);
     Task<(IEnumerable<BusinessPartnerSAPDTO> Data, int Count)> GetVendorsAsync(DataGridIntent intent);
     Task<(IEnumerable<BusinessPartnerSAPDTO> Data, int Count)> GetAllAsync(DataGridIntent intent);
 }
