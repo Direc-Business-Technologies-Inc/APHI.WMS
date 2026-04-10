09597ed5ec4bbd754de6efb7dd5acf877d4a7d15
tbsi-nicolas <tbsi.nicolas@gmail.com>
2026-04-10 13:12:53 +0800

    fix: SAP document does not have PrepBy UDF

--- Integration.SAP/Entities/Transactional/InventoryCounting/InventoryCountingsPayload.cs
+++ Integration.SAP/Entities/Transactional/InventoryCounting/InventoryCountingsPayload.cs
@@ -5,7 +5,7 @@ namespace Integration.SAP.Entities.Transactional.InventoryCounting;
 public class InventoryCountingsPayload
 {
     public DateTime CountDate { get; private set; }
-    public string U_PrepBy { get; private set; }
+    //public string U_PrepBy { get; private set; }
     public string Remarks { get; private set; }
     public List<InventoryCountingsLinesPayload> InventoryCountingLines { get; private set; } = [];
 
@@ -13,7 +13,7 @@ public class InventoryCountingsPayload
     {
         CountDate = Guard.Against.NullOrOutOfSQLDateRange(cntDate, nameof(CountDate), "Count Date must be a valid Date");
         InventoryCountingLines = [.. Guard.Against.NullOrEmpty(lines, nameof(InventoryCountingLines), "Inventory Counting Lines cannot be null or empty")];
-        U_PrepBy = prepBy;
+        //U_PrepBy = prepBy;
         Remarks = remarks;
     }
 }
