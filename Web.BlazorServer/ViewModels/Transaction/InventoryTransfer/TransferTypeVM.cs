bc7c36997259cf5fbfef8b95ff7b6337449a3709
tbsirupert <tbsi.rupert@gmail.com>
2026-05-29 11:48:03 +0800

    fix: [IL_ID_142] - made fields nullable-aware

--- Web.BlazorServer/ViewModels/Transaction/InventoryTransfer/TransferTypeVM.cs
+++ Web.BlazorServer/ViewModels/Transaction/InventoryTransfer/TransferTypeVM.cs
@@ -2,6 +2,6 @@
 
 public class TransferTypeVM
 {
-    public string Name { get; set; }
-    public string Code { get; set; }    
+    public string Name { get; set; } = string.Empty;
+    public string Code { get; set; } = string.Empty;
 }
