6d22e436eada7b2ff98f13ead68ecb67d680d13a
tbsirupert <tbsi.rupert@gmail.com>
2026-06-23 12:46:40 +0800

    fix: [IL_ID_169] - add custom text to prompt when removing lines in sales return

--- Web.BlazorServer/Components/Pages/Transaction/SalesReturn/SalesReturnCVUPage.razor.cs
+++ Web.BlazorServer/Components/Pages/Transaction/SalesReturn/SalesReturnCVUPage.razor.cs
@@ -351,7 +351,7 @@ public partial class SalesReturnCVUPage
 
     async Task RemoveLine(SalesReturnLineVM item)
     {
-        if (!await AlertService.PromptAsync())
+        if (!await AlertService.PromptAsync("Remove line from sales return?", "Remove Line?"))
             return;
 
         FormData.DocumentLines.Remove(item);
