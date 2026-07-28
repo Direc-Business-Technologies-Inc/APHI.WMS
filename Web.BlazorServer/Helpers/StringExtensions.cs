1d2ca0d906e4dfb14185139d20724d15cdf8129a
tbsirupert <tbsi.rupert@gmail.com>
2026-07-28 16:15:19 +0800

    Squashed commit of the following:
    commit 9586a6670cc0680d637935b15ce691b81b77fdf8
    Author: tbsirupert <tbsi.rupert@gmail.com>
    Date:   Tue Jul 28 16:14:54 2026 +0800
    
        feat: sales return - responsive columns
    
    commit 333432f7001041546dac780d1fcb492820e62f32
    Author: tbsirupert <tbsi.rupert@gmail.com>
    Date:   Tue Jul 28 16:12:11 2026 +0800
    
        feat: goods receipt - responsive columns
    
    commit a86b2dac9f1bcd65776efdb44fdb6055f4c5e0cc
    Author: tbsirupert <tbsi.rupert@gmail.com>
    Date:   Tue Jul 28 16:08:06 2026 +0800
    
        feat: inventory transfer request - responsive columns
    
    commit 3b6ea480cb517b531b33fe12e8cd44d9195868a5
    Author: tbsirupert <tbsi.rupert@gmail.com>
    Date:   Tue Jul 28 16:01:52 2026 +0800
    
        feat: inventory counting  - responsive columns
    
    commit fed5eec195871690f177130b917fe5195945faf5
    Author: tbsirupert <tbsi.rupert@gmail.com>
    Date:   Tue Jul 28 15:52:58 2026 +0800
    
        feat: goods return - responsive columns
    
    commit 239475ceeba5d0bd81450fb952f4856d16f42267
    Author: tbsirupert <tbsi.rupert@gmail.com>
    Date:   Tue Jul 28 15:46:36 2026 +0800
    
        feat: goods receipt - responsive columns
    
    commit 4382ff3fced77fb950467975a51cb0b783b8e126
    Author: tbsirupert <tbsi.rupert@gmail.com>
    Date:   Tue Jul 28 15:34:06 2026 +0800
    
        feat: good issue - responsive ui
    
    commit fbf476cb6fa04f41fc028971800d1ce8f84746e2
    Author: tbsirupert <tbsi.rupert@gmail.com>
    Date:   Tue Jul 28 14:56:48 2026 +0800
    
        feat: Delivery - responsive UI
    
    commit 2e2b9cecca2aded96b9e5c847b165e6a88062173
    Author: tbsirupert <tbsi.rupert@gmail.com>
    Date:   Tue Jul 28 13:00:45 2026 +0800
    
        fix: app body padding too big

---
+++ Web.BlazorServer/Helpers/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Web.BlazorServer.Helpers
+{
+    public static class StringExtensions
+    {
+        public static string OrWhenNull(this string? a, string alternative)
+        {
+            if (string.IsNullOrEmpty(a)) return alternative;
+            return a;
+        }
+    }
+}
